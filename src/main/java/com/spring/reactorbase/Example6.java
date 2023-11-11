99adf3bd2111892a4000a87072e18a57a6d3b86d
clearmirror-js <kangjisu0403@gmail.com>
2023-11-11 23:34:07 +0900

    implement example

--- src/main/java/com/spring/reactorbase/Example6.java
+++ src/main/java/com/spring/reactorbase/Example6.java
@@ -2,13 +2,13 @@ package com.spring.reactorbase;
 
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
-import org.springframework.boot.json.JsonParser;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.net.URI;
@@ -17,9 +17,13 @@ import java.util.Collections;
 public class Example6 {
 
     public static void main(String[] args) {
-        helloReactor();
+        monoHelloReactor();
         monoEmpty();
-        worldTime();
+        monoWorldTime();
+        fluxJust();
+        fluxFromArray();
+        monoToFlux();
+        fluxConcat();
     }
 
     private static void monoEmpty() {
@@ -33,7 +37,7 @@ public class Example6 {
         System.out.println();
     }
 
-    private static void helloReactor() {
+    private static void monoHelloReactor() {
         System.out.println("Example6.helloReactor");
         Mono.just("Hello Reactor")
             .map(String::toUpperCase)
@@ -41,7 +45,7 @@ public class Example6 {
         System.out.println();
     }
 
-    private static void worldTime() {
+    private static void monoWorldTime() {
         System.out.println("Example6.worldTime");
         URI worldTimeUri = UriComponentsBuilder.newInstance().scheme("http")
             .host("worldtimeapi.org")
@@ -55,6 +59,7 @@ public class Example6 {
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
 
+        // non-blocking I/O 방식의 통신은 아니나, Mono를 활용하여 요청과 응답을 하나의 Operator 체인으로 처리할 수 있음
         Mono.just(
                 restTemplate.exchange(
                     worldTimeUri,
@@ -78,5 +83,45 @@ public class Example6 {
             );
 
         System.out.println();
+    }
+
+    private static void fluxJust() {
+        System.out.println("Example6.fluxJust");
+        Flux.just(6, 9, 13)
+            .map(num -> num % 2)
+            .subscribe(System.out::println);
+        System.out.println();
+    }
+
+    private static void fluxFromArray() {
+        System.out.println("Example6.fluxFromArray");
+        Flux.fromArray(new Integer[]{3, 6, 7, 9})
+            .filter(num -> num > 6)
+            .map(num -> num * 2)
+            .subscribe(System.out::println);
+        System.out.println();
+    }
+
+    private static void monoToFlux() {
+        System.out.println("Example6.monoToFlux");
+        // 데이터 소스를 하나로 연결하여 새로운 flux publisher 생성
+        Flux<String> flux = Mono.justOrEmpty("Steve")
+            .concatWith(Mono.justOrEmpty("Jobs"));
+        flux.subscribe(System.out::println);
+        System.out.println();
+    }
+
+    private static void fluxConcat() {
+        Flux
+            // 여러 데이터 소스를 emit하는 flux를 생성
+            .concat(
+                Flux.just("Mercury", "Venus", "Earth"),
+                Flux.just("Mars", "Jupiter", "Saturn"),
+                Flux.just("Uranus", "Neptune", "Pluto")
+            )
+            // 여러 데이터 소스를 하나의 리스트로 병합
+            // 결과로는 Mono가 생성
+            .collectList()
+            .subscribe(planets -> System.out.println("planets = " + planets));
     }
 }
